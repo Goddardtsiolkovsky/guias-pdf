b025748815fec29ac8deda51fe3969e5d8aaf850
Goddardtsiolkovsky <brayan.ortiz@ug.uchile.cl>
2025-02-07 20:30:14 -0300

    Nueva Home

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-// Función para actualizar la fecha en el encabezado
-function actualizarFecha() {
-    const fechaActual = new Date();
-    const opciones = { day: 'numeric', month: 'long', year: 'numeric' };
-    const fechaFormateada = fechaActual.toLocaleDateString('es-ES', opciones);
-    document.getElementById('fecha-actual').textContent = fechaFormateada;
-  }
-  
-  // Llamar a la función cuando cargue la página
-  actualizarFecha();
-  
-  // Script para manejar la navegación entre secciones
-  document.querySelectorAll('.nav-bar a').forEach(link => {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      document.querySelectorAll('.section').forEach(section => {
-        section.classList.remove('active');
-      });
-      document.querySelector(this.getAttribute('href')).classList.add('active');
+// Función para actualizar la fecha y hora en el encabezado
+function actualizarFechaHora() {
+  const fechaActual = new Date();
+  const opcionesFecha = { day: 'numeric', month: 'long', year: 'numeric' };
+  const fechaFormateada = fechaActual.toLocaleDateString('es-ES', opcionesFecha);
+  document.getElementById('fecha-actual').textContent = fechaFormateada;
+
+  const horaFormateada = fechaActual.toLocaleTimeString('es-ES');
+  document.getElementById('hora-actual').textContent = horaFormateada;
+}
+
+// Llamar a la función cuando cargue la página y cada segundo
+actualizarFechaHora();
+setInterval(actualizarFechaHora, 1000);
+
+// Script para manejar la navegación entre secciones
+document.querySelectorAll('.nav-bar a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelectorAll('.section').forEach(section => {
+      section.classList.remove('active');
     });
+    document.querySelector(this.getAttribute('href')).classList.add('active');
   });
-  
-  // Mostrar la sección de Home por defecto
-  document.querySelector('#home').classList.add('active');
-  
+});
+
+// Mostrar la sección de Home por defecto
+document.querySelector('#home').classList.add('active');
